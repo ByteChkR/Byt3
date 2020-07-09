06a2f6ad029cf98fd05909ac64732fb23d00f51e
Tim Akermann <ti.akermann@gmail.com>
2020-07-09 08:35:29 +0200

    [skip travis] Fixed wrong resource name

--- Applications/FLDebugger.Projects/Properties/AssemblyInfo.cs
+++ Applications/FLDebugger.Projects/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.91.402")]
-[assembly: AssemblyFileVersion("1.2.91.402")]
+[assembly: AssemblyVersion("1.2.92.23")]
+[assembly: AssemblyFileVersion("1.2.92.23")]
